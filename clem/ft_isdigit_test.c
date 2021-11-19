33d3687517513daad3edf5c2e8c08fa5c99eb416
Clement Cariou <clem@Clements-MacBook-Air.local>
2021-11-19 10:47:02 +0200

    first draft

---
+++ clem/ft_isdigit_test.c
@@ -0,0 +1,18 @@
+void	isdigit()
+{
+	char c = '9';
+
+	int a = c;
+	printf("%c isdigit = %d ft_isdigit = %d\n", c, isdigit(a), ft_isdigit(a));
+	return (0);
+}
+
+void	ft_isdigit_test()
+{
+	ft_printsep(15);
+	ft_newline();
+	ft_putendl("FT_ISDIGIT_TEST");
+	isdigit_test();
+	ft_newline();
+	ft_printsep(15);
+}
